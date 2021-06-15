f86fe0efb44bf0f69f6e1948abcbd557954656ee
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-06-15 18:57:19 -0300

    Navbar elements

---
+++ src/components/Navbar/Navbar.elements.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { Container } from '../../GlobalStyles'
+import { Link } from 'react-router-dom'
+import { FaMagento } from 'react-icons/fa'
+
+export const Nav = styled.nav`
+  background: #101522;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+
+  &:hover {
+    
+  }
+`;
+
+export const NavbarContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+
+  ${Container}
+`;
+
+export const NavLogo = styled(Link)`
+  color: #fff;
+  justify-self: flex-start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+`;
+
+export const NavIcon = styled(FaMagento)`
+  margin-right: 0.5rem;
+`;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Nav, NavbarContainer, NavLogo, NavIcon } from './Navbar.elements';
+
+
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <NavIcon />
+            ULTRA
+          </NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
