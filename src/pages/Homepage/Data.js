c0f077b37ee47b75e394bed4dd599fe76bc09d29
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-06-16 20:06:14 -0300

    InfoSection

--- src/pages/Homepage/Data.js
+++ src/pages/Homepage/Data.js
@@ -1,3 +1,68 @@
+import svg1 from '../../images/svg1.svg';
+import svg2 from '../../images/svg2.svg';
+import svg3 from '../../images/svg3.svg';
+import profile from '../../images/profile.jpg';
+
 export const homeObjOne = {
-  lightBg: false
+  lightBg: false,
+  primary: true,
+  imgStart: '',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Marketing Agency',
+  headline: 'Lead Generation Spacialist for Online Businesses',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help your business goals',
+  buttonLabel: 'Get Started',
+  img: svg1,
+  alt: 'Image',
+  start: '',
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: 'start',
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Marketing Agency',
+  headline: 'Lead Generation Spacialist for Online Businesses',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help your business goals',
+  buttonLabel: 'Get Started',
+  img: profile,
+  alt: 'Image',
+  start: 'True',
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  primary: true,
+  imgStart: '',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Marketing Agency',
+  headline: 'Lead Generation Spacialist for Online Businesses',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help your business goals',
+  buttonLabel: 'Get Started',
+  img: svg2,
+  alt: 'Image',
+  start: '',
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  primary: true,
+  imgStart: 'start',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Marketing Agency',
+  headline: 'Lead Generation Spacialist for Online Businesses',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help your business goals',
+  buttonLabel: 'Get Started',
+  img: svg3,
+  alt: 'Image',
+  start: 'true',
 };
