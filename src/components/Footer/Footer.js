08b14cf6301e5ed9c7e8cc659c75fa2191ae1fb2
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-06-17 05:27:02 -0300

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import { Button } from '../../GlobalStyles';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
 
 import {
   FooterContainer,
@@ -12,7 +19,14 @@ import {
   FooterLinksWrapper,
   FooterLinkItems,
   FooterLinkTitle,
-  FooterLink
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcon,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink
 } from './Footer.elements';
 
 const Footer = () => {
@@ -40,6 +54,7 @@ const Footer = () => {
             <FooterLink to='/'>Investors</FooterLink>
             <FooterLink to='/'>Terms of Service</FooterLink>
           </FooterLinkItems>
+
           <FooterLinkItems>
             <FooterLinkTitle>Contact Us</FooterLinkTitle>
             <FooterLink to='/'>Contact</FooterLink>
@@ -48,6 +63,7 @@ const Footer = () => {
             <FooterLink to='/'>Sponsorships</FooterLink>
           </FooterLinkItems>
         </FooterLinksWrapper>
+        
         <FooterLinksWrapper>
           <FooterLinkItems>
             <FooterLinkTitle>Videos</FooterLinkTitle>
@@ -56,6 +72,7 @@ const Footer = () => {
             <FooterLink to='/'>Agency</FooterLink>
             <FooterLink to='/'>Influencer</FooterLink>
           </FooterLinkItems>
+
           <FooterLinkItems>
             <FooterLinkTitle>Social Media</FooterLinkTitle>
             <FooterLink to='/'>Instagram</FooterLink>
@@ -65,6 +82,41 @@ const Footer = () => {
           </FooterLinkItems>
         </FooterLinksWrapper>
       </FooterLinksContainer>
+
+      <SocialMedia>
+        <SocialMediaWrap>
+
+          <SocialLogo to='/'>
+            <SocialIcon />
+            ULTRA
+          </SocialLogo>
+
+          <WebsiteRights>ULTRA 2021</WebsiteRights>
+
+          <SocialIcons>
+            <SocialIconLink href='/' target='_blank' arial-label="Facebook">
+              <FaFacebook />
+            </SocialIconLink>
+
+            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+              <FaInstagram />
+            </SocialIconLink>
+
+            <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+              <FaYoutube />
+            </SocialIconLink>
+
+            <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+              <FaTwitter />
+            </SocialIconLink>
+
+            <SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+
+        </SocialMediaWrap>
+      </SocialMedia>
 
     </FooterContainer>
   )
